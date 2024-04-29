e6113833a316a30ba6b58bbfcd7f70ecc4a068b7
elenarecio <erp1004@alu.ubu.es>
2024-04-29 19:46:31 +0200

    Cambiar botones de la pantalla de inicio y añadir la pantalla del menu

--- app/src/main/java/com/example/tfg/MainActivity.java
+++ app/src/main/java/com/example/tfg/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.menu);
         boton=this.findViewById(R.id.Boton);
     }
 
