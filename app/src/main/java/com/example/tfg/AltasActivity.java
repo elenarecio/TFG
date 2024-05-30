23e7a523405b31b6383b643412c70b92a72f5640
elenarecio <erp1004@alu.ubu.es>
2024-05-30 20:56:36 +0200

    Creación de las vistas
    Creación de todas las vistas de la aplicación, en una primera idea. Es posible que se añadan más

--- app/src/main/java/com/example/tfg/AltasActivity.java
+++ app/src/main/java/com/example/tfg/AltasActivity.java
@@ -1,6 +1,10 @@
 package com.example.tfg;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,34 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AltasActivity extends AppCompatActivity {
+    Button boton11, boton10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_altas);
+        super.onCreate(savedInstanceState);
+        boton11 = findViewById(R.id.boton11);
+        boton10 = findViewById(R.id.boton10);
+        EdgeToEdge.enable(this);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+        boton11.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent i = new Intent(AltasActivity.this, Nacimientos.class);
+                startActivity(i);
+            }
+        });
+
+        boton10.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent i = new Intent(AltasActivity.this, Compras.class);
+                startActivity(i);
+            }
         });
     }
 }
--- app/src/main/java/com/example/tfg/BajasActivity.java
+++ app/src/main/java/com/example/tfg/BajasActivity.java
@@ -1,6 +1,9 @@
 package com.example.tfg;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class BajasActivity extends AppCompatActivity {
+    Button boton12, boton13;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_bajas);
+        boton13 = findViewById(R.id.boton13);
+        boton12 = findViewById(R.id.boton12);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        boton13.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent i = new Intent(BajasActivity.this, Muertes.class);
+                startActivity(i);
+            }
+        });
+        boton12.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent i = new Intent(BajasActivity.this, Ventas.class);
+                startActivity(i);
+            }
         });
     }
 }
